93266e771e6bcb47d9d96cb8cf00aad3de8b02ce
francisco_figueiredo <francisco_figueiredo@hotmail.com>
2016-02-08 19:16:47 -0200

    Primeiros formulários

---
+++ Ingles_Sem_Mestre/frm00_Principal.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Ingles_Sem_Mestre
+{
+    partial class frm00_Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnLicoes_Secoes_ListaTraducao = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnLicoes_Secoes_ListaTraducao);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(898, 587);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnLicoes_Secoes_ListaTraducao
+            // 
+            this.btnLicoes_Secoes_ListaTraducao.Image = global::Ingles_Sem_Mestre.Properties.Resources.Licoes;
+            this.btnLicoes_Secoes_ListaTraducao.Location = new System.Drawing.Point(3, 3);
+            this.btnLicoes_Secoes_ListaTraducao.Name = "btnLicoes_Secoes_ListaTraducao";
+            this.btnLicoes_Secoes_ListaTraducao.Size = new System.Drawing.Size(211, 110);
+            this.btnLicoes_Secoes_ListaTraducao.TabIndex = 0;
+            this.btnLicoes_Secoes_ListaTraducao.Text = "Lições -> Seções -> Lista de Tradução";
+            this.btnLicoes_Secoes_ListaTraducao.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnLicoes_Secoes_ListaTraducao.UseVisualStyleBackColor = true;
+            // 
+            // frm00_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 587);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "frm00_Principal";
+            this.Text = "Inglês Sem Mestre - Principal";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnLicoes_Secoes_ListaTraducao;
+    }
+}
---
+++ Ingles_Sem_Mestre/frm01_Licoes_Secoes_ListaTraducoes.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Ingles_Sem_Mestre
+{
+    partial class frm01_Licoes_Secoes_ListaTraducoes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm01_Licoes_Secoes_ListaTraducoes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 602);
+            this.Name = "frm01_Licoes_Secoes_ListaTraducoes";
+            this.Text = "Inglês sem Mestre - Lições -> Seções -> Lista de Traduções";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
